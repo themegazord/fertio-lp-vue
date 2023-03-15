812705c1ad8eddcd7217d228902baeaf640b288a
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-03-15 06:51:52 -0400

    chore: realizado a resolução 1366x900

--- src/views/Produtos/ProdutosView.vue
+++ src/views/Produtos/ProdutosView.vue
@@ -368,4 +368,86 @@ export default {
       font-size: .884rem;
     }
   }
+  @media screen and (max-width: 1600px){
+    #produtos {
+      width: 100%;
+      height: 100vh;
+      background-color: #FFFFFF;
+    }
+
+    #produtos-main-container {
+      display: grid;
+      grid-template-columns: 37.5% 62.5%;
+      height: 100%;
+    }
+
+    #left-produtos-main-container {
+      max-height: calc(100vh - 90px);
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      padding: 4.269rem 5.87rem 0 2.846rem;
+    }
+
+    #title-left-produtos {
+      font-size: 2.402rem;
+      line-height: 2.775rem;
+      font-weight: 700;
+      color: #575757;
+    }
+
+    #title-left-produtos span {
+      color: #019A34;
+    }
+
+    #redes-sociais-produtos {
+      display: flex;
+      font-size: 2.242rem;
+      gap: 1.067rem;
+      margin-bottom: 2.846rem;
+    }
+
+    #right-produtos-main-container {
+      max-height: calc(100vh - 90px);
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      /* gap: 3.8rem; */
+      margin: 2.668rem 3.202rem 8rem 0;
+    }
+
+    #thiosul,
+    #cats,
+    #kts {
+      display: flex;
+      gap: 1.335rem;
+    }
+
+    #thiosul > p,
+    #cats > p,
+    #kts > p {
+      font-size: 1.067rem;
+      line-height: 1.335rem;
+      font-weight: 400;
+      color: #383A47;
+      opacity: .8;
+      text-align: justify;
+    }
+
+    #thiosul > img,
+    #cats > img,
+    #kts > img {
+      width: 10.939rem;
+      height: 8.004rem;
+    }
+
+    #cats {
+      flex-direction: row-reverse;
+      text-align: right;
+    }
+
+    #kts > p{
+      font-size: .839rem;
+    }
+  }
 </style>
